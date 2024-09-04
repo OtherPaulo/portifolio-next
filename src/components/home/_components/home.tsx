05c6612b9289af407e72625b3a20b064681d83ac
Paulo Rodrigues <paulovinicius356@gmail.com>
2024-09-04 17:04:58 -0300

    att style home

--- src/components/home/_components/home.tsx
+++ src/components/home/_components/home.tsx
@@ -6,9 +6,9 @@ export function Home() {
 
   return (
     <div className="h-[12rem] flex justify-center items-center px-4">
-      <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
+      <div className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
         Olá, eu sou Paulo! <br />
-        Seu desenvolvedor
+        Seu desenvolvedor {" "}
         <FlipWords words={words} /> <br />
       </div>
     </div>
