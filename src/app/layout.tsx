7bf25b1e4470f91238cadddc0938218065faeb26
Paulo Rodrigues <paulovinicius356@gmail.com>
2024-09-05 01:46:12 -0300

    texte image preview in link share

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,34 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Paulo Dev",
   description: "Seu Desenvolvedor FullStack",
+  openGraph: {
+    title: "Paulo Dev",
+    description: "Seu Desenvolvedor FullStack",
+    url: "https://www.portifolio.paulorodrigues.tech", // Coloque o URL do seu site
+    siteName: "Paulo Dev",
+    images: [
+      {
+        url: "https://www.portifolio.paulorodrigues.tech/preview.png", // URL da imagem que você quer como preview
+        width: 800,
+        height: 600,
+        alt: "Paulo Dev - FullStack Developer",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@seuTwitterHandle",
+    title: "Paulo Dev",
+    description: "Seu Desenvolvedor FullStack",
+    images: ["https://www.portifolio.paulorodrigues.tech/preview.png"], // Mesma imagem ou outra para Twitter
+  },
 };
 
 export default function RootLayout({
@@ -19,15 +41,15 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={inter.className}>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-            </ThemeProvider>
-            </body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
