b48e2858f783d66e1cbabcefd212e7349dbec14d
Paulo Rodrigues <paulo.rodrigues@enablers.com.br>
2026-01-24 16:44:20 -0400

    Refactor SVG elements in Sobre component for consistency and clarity

--- src/components/sobre/_components/sobre.tsx
+++ src/components/sobre/_components/sobre.tsx
@@ -118,7 +118,7 @@ export function Sobre() {
             </svg>
 
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0,0,256,256">
-            <g fill="#6a6a6a" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none"><g transform="scale(8.53333,8.53333)">
+            <g fill="#6a6a6a" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none"><g transform="scale(8.53333,8.53333)">
               <path d="M15,3c-6.627,0 -12,5.373 -12,12c0,5.623 3.872,10.328 9.092,11.63c-0.056,-0.162 -0.092,-0.35 -0.092,-0.583v-2.051c-0.487,0 -1.303,0 -1.508,0c-0.821,0 -1.551,-0.353 -1.905,-1.009c-0.393,-0.729 -0.461,-1.844 -1.435,-2.526c-0.289,-0.227 -0.069,-0.486 0.264,-0.451c0.615,0.174 1.125,0.596 1.605,1.222c0.478,0.627 0.703,0.769 1.596,0.769c0.433,0 1.081,-0.025 1.691,-0.121c0.328,-0.833 0.895,-1.6 1.588,-1.962c-3.996,-0.411 -5.903,-2.399 -5.903,-5.098c0,-1.162 0.495,-2.286 1.336,-3.233c-0.276,-0.94 -0.623,-2.857 0.106,-3.587c1.798,0 2.885,1.166 3.146,1.481c0.896,-0.307 1.88,-0.481 2.914,-0.481c1.036,0 2.024,0.174 2.922,0.483c0.258,-0.313 1.346,-1.483 3.148,-1.483c0.732,0.731 0.381,2.656 0.102,3.594c0.836,0.945 1.328,2.066 1.328,3.226c0,2.697 -1.904,4.684 -5.894,5.097c1.098,0.573 1.899,2.183 1.899,3.396v2.734c0,0.104 -0.023,0.179 -0.035,0.268c4.676,-1.639 8.035,-6.079 8.035,-11.315c0,-6.627 -5.373,-12 -12,-12z"></path></g></g>
             </svg>
 
@@ -131,7 +131,7 @@ export function Sobre() {
             </svg>
 
             <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="50" height="50" viewBox="0,0,256,256">
-            <g fill="#1a41c1" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none" textAnchor="none"><g transform="scale(5.12,5.12)">
+            <g fill="#1a41c1" fillRule="nonzero" stroke="none" strokeWidth="1" strokeLinecap="butt" strokeLinejoin="miter" strokeMiterlimit="10" strokeDasharray="" strokeDashoffset="0" fontFamily="none" fontWeight="none" fontSize="none"><g transform="scale(5.12,5.12)">
               <path d="M25,2c-12.682,0 -23,10.317 -23,23c0,0.553 0.448,1 1,1h7c0.552,0 1,-0.448 1,-1c0,-7.732 6.268,-14 14,-14c7.732,0 14,6.268 14,14c0,7.732 -6.268,14 -14,14v-7c0,-0.552 -0.448,-1 -1,-1h-7c-0.552,0 -1,0.448 -1,1v7h-5v-4c0,-0.552 -0.448,-1 -1,-1h-4c-0.552,0 -1,0.448 -1,1v4c0,0.552 0.448,1 1,1h4v5c0,0.552 0.448,1 1,1h5c0.552,0 1,-0.448 1,-1v-5h7v7c0,0.553 0.448,1 1,1c12.682,0 23,-10.317 23,-23c0,-12.683 -10.318,-23 -23,-23z"></path></g></g>
             </svg>
           </motion.div>
